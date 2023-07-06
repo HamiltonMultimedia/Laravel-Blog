edb3767736ce98aaf16171b5c2a2efed30a47da9
HamiltonMultimedia <24661651+HamiltonMultimedia@users.noreply.github.com>
2023-07-06 11:15:59 -0400

    updated deploy

--- deploy.php
+++ deploy.php
@@ -5,11 +5,14 @@ require 'recipe/laravel.php';
 
 // Config
 
-set('repository', 'git@github.com:HamiltonMultimedia/Laravel-Blog.git');
+set('default_stage', 'staging');
 
 add('shared_files', []);
 add('shared_dirs', []);
 add('writable_dirs', []);
+
+set('repository', 'git@github.com:HamiltonMultimedia/Laravel-Blog.git');
+set('branch', 'dev');
 
 // Hosts
 
