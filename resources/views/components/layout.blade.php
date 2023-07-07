6fe94ee8bad26326e129cbf2106537ec0b78d838
HamiltonMultimedia <24661651+HamiltonMultimedia@users.noreply.github.com>
2023-07-06 23:40:33 -0400

    darkside lary

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -92,7 +92,7 @@
         <footer id="newsletter"
                 class="bg-gray-800 border border-black border-opacity-5 rounded-xl text-center py-16 px-10 mt-16"
         >
-            <img src="/images/lary-newsletter-icon.svg" alt="" class="mx-auto -mb-6" style="width: 145px;">
+            <img src="/images/dark-lary-newsletter-icon.png" alt="" class="mx-auto -mb-6" style="width: 145px;">
 
             <h5 class="text-3xl text-gray-400">Stay in touch with the latest posts</h5>
             <p class="text-sm mt-3 text-gray-400">Promise to keep the inbox clean. No bugs.</p>
